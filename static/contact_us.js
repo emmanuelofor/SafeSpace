7bcfa70230813d190cd10fbdc2d2f9d4d9d18b3e
emmanuelofor <emmanuel.ofori@kibo.school>
2023-06-11 16:34:36 +0000

    javascript for contact us updated

--- static/contact_us.js
+++ static/contact_us.js
@@ -1,17 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // Find the send message button in the document
-    const sendMessageButton = document.querySelector('.contact-form .btn');
+    // Find the form in the document
+    const form = document.querySelector('.contact-form');
 
-    // Listen for a click event on the send message button
-    sendMessageButton.addEventListener('click', function(event) {
+    // Listen for a submit event on the form
+    form.addEventListener('submit', function(event) {
         
-        // Prevent the default action of the click event (like navigating to another page)
-        event.preventDefault();
-        
-        // Display the thank you message
-        alert('Thank you for contacting us. We will get back to you soon');
+        // Check if all fields are filled
+        const inputs = form.querySelectorAll('input, textarea');
+        let allFilled = true;
+
+        inputs.forEach(input => {
+            if (input.value.trim() === '') {
+                allFilled = false;
+            }
+        });
+
+        if (!allFilled) {
+            // Prevent the form from being submitted if any field is not filled
+            event.preventDefault();
+            
+            // Optionally show an alert or some other form of message
+            alert('Please fill all fields before submitting');
+        } else {
+            // Display the thank you message
+            alert('Thank you for contacting us. We will get back to you soon');
+        }
         
     });
 
 });
+
